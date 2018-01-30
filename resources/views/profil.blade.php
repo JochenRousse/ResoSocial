19d41ebd900e2f05dd11288f4b3d70b704abd2df
Johann Carfantan <jojohann22550@gmail.com>
2018-01-30 13:56:01 +0100

    update view profil

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -4,8 +4,14 @@
 
     <div class="col-md-10">
         <div class="view-container container">
-            salut à tous les amis c david la farge
-            Mon id est : {{Auth::user()->id}}
+            <h1>{{Auth::user()->nom}} {{Auth::user()->prenom}}</h1>
+            <hr/>
+            <p>{{Auth::user()->genre}}</p>
+            <p>{{Auth::user()->ddn}}</p>
+            <p>{{Auth::user()->email}}</p>
+            <hr/>
+            <h1>Mes posts</h1>
+            <p>Ici la liste de mes posts... + commentaires</p>
         </div>
     </div>
 
