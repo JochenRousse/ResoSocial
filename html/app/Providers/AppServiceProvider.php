f0b01ab0a90780f3c2519b7f20e561eab1e1a253
Johann Carfantan <jojohann22550@gmail.com>
2018-02-04 18:21:48 +0100

    Event resolution probleme

---
+++ html/app/Providers/AppServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //Uncomment if you don't wish to cache all users
+        $this->app->bind(
+            'App\Repositories\User\UserRepository',
+            'App\Repositories\User\EloquentUserRepository'
+        );
+
+        $this->app->bind(
+            'App\Repositories\FriendRequest\FriendRequestRepository',
+            'App\Repositories\FriendRequest\EloquentFriendRequestRepository'
+        );
+
+        $this->app->bind(
+            'App\Repositories\Group\GroupRepository',
+            'App\Repositories\Group\EloquentGroupRepository'
+        );
+
+        $this->app->bind(
+            'App\Repositories\Event\EventRepository',
+            'App\Repositories\Event\EloquentEventRepository'
+        );
+    }
+}
